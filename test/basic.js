a1d395d413c5cddc5caa3f0e78070aa68b509b9b
Rasso Hilber <mail@rassohilber.com>
2018-09-06 13:24:54 +0200

    Add Custom Asset “mdrnzr”

--- test/basic.js
+++ test/basic.js
@@ -1,34 +1,58 @@
+const assertBundleTree = require('parcel-assert-bundle-tree');
 const assert = require('assert');
 const { setupBundler } = require('./utils');
-const ModernizrPlugin = require('../lib/index');
 const path = require('path');
 const fs = require('fs-extra');
 
-describe('basic', function() {
-  it('Should create a modernizr build', async function() {
-    try {
-      await fs.emptyDir( path.join(__dirname, './dist') )
-      assert( true );
-    } catch (err) {
-      console.error(err)
-    }
-    const bundler = await setupBundler(path.join(__dirname, './Integration/Basic/index.html'), {});
-    bundler.on('bundled', () => {
-      assert( fs.existsSync( path.join(__dirname, './dist/modernizr-custom.js') ) );
-    })
-    const bundle = await bundler.bundle();
-  });
-  it('Should load the modernizr config', async function() {
-    let config = ModernizrPlugin.getConfig();
-    assert( typeof config === 'object' || typeof config === false );
-  });
-  it('Should load the modernizr config from .modernizrrc', async function() {
-    let config = ModernizrPlugin.getConfigFromFile( `${process.cwd()}/.modernizrrc` );
-    assert( typeof config === 'object' || typeof config === false );
-  });
-  it('Should load the modernizr config from package.json', async function() {
-    let config = ModernizrPlugin.getConfigFromPackage( `${process.cwd()}/package.json` );
-    assert( typeof config === 'object' || typeof config === false );
-  });
+fs.emptyDirSync( path.join(__dirname, './dist') )
 
-});
+function test() {
+  describe('Only Script', function() {
+    it('Should compile checker.js', async function() {
+      const bundler = await setupBundler(path.join(__dirname, './Integration/Basic/checker.js'), {});
+      const bundle = await bundler.bundle();
+      // Compare bundle to expected
+      assertBundleTree(bundle, {
+        type: 'js',
+        name: 'checker.js',
+        assets: ['checker.js'],
+        childBundles: undefined
+      });
+    });
+  });
+  describe('Direct', function() {
+    it('Should create a modernizr build from a modernizr.js as entryPoint', async function() {
+      const bundler = await setupBundler(path.join(__dirname, './Integration/Basic/modernizr.mdrnzr'), {});
+      const bundle = await bundler.bundle();
+      // Compare bundle to expected
+      assertBundleTree(bundle, {
+        type: 'js',
+        name: 'modernizr.js',
+        assets: ['modernizr.mdrnzr'],
+        childBundles: []
+      });
+    });
+  });
+  describe('Embedded', function() {
+    it('Should create a modernizr build from a modernizr.mdrnzr embedded in an index.html', async function() {
+      const bundler = await setupBundler(path.join(__dirname, './Integration/Basic/index.html'), {});
+      const bundle = await bundler.bundle();
+      // Compare bundle to expected
+      assertBundleTree(bundle, {
+        type: 'html',
+        name: 'index.html',
+        assets: ['index.html'],
+        childBundles: [
+          {
+            type: 'js'
+          },
+          {
+            type: 'js'
+          }
+        ]
+      });
+    });
+  })
+}
+
+test();
